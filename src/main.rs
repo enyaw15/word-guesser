05de4012ed11640e5fefd4e71f84a480edcd00b9
enyaw15 <enyaw0215@gmail.com>
2022-07-31 20:48:38 -0700

    created basic word guessing game
    the secret word is hello
    the player guesses letters until they have the whole word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::io;
+use std::collections::HashSet;
 fn main() {
-    println!("Hello, world!");
+    let words = ["hello", "world", "potato", "racecar", "tiger", "string", "rust" , "quiz"];
+    let word = words[0];
+    let word_len = word.len();
+    let mut guesses:HashSet<char> = HashSet::new();
+    let mut guess_count = 0;
+    
+
+    println!("Word length: {word_len}");
+    loop {
+        let mut victory = true;
+        for letter in word.chars() {
+            if guesses.contains(&letter) {
+                print!("{letter}");
+            } else {
+                print!("_");
+                victory = false;
+            }
+        }
+        println!("");
+
+        if victory {
+            println!("You win!");
+            return;
+        }
+    
+        println!("Enter a letter to guess!");
+        guess_count += 1;
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let guess:char = input.chars().next().unwrap();
+        if guesses.contains(&guess)  {
+            println!("You already guessed that letter");
+        } else {
+            guesses.insert(guess);
+        }
+        
+
+        
+    }
 }
